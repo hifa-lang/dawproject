800eff5b375ccb20f9f240caeaae3630181fc94e
kurotakazuki <kabisakanakabi@gmail.com>
2024-04-05 18:06:32 +0900

    add: pub use of MetaData and Project

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,8 @@ mod read;
 mod repositories;
 
 pub use read::{DawprojectReadError, DawprojectReader};
+/// Metadata of the `metadata.xml` file.
+pub use repositories::metadata::MetaData;
+/// Project of the `project.xml` file.
+pub use repositories::project::Project;
 pub use repositories::*;
