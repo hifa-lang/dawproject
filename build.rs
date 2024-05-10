6ab3a5f9076af235cb84c286efea29dfafd6a38e
kurotakazuki <kabisakanakabi@gmail.com>
2024-05-10 18:18:03 +0900

    update: version

--- build.rs
+++ build.rs
@@ -1,11 +1,13 @@
-// const PROJECT_XSD: &str = include_str!("dawproject/Project.xsd");
-
 fn main() {
-    // let fixed_project_xsd = PROJECT_XSD.replace(
-    //     "<xs:attribute name=\"contentType\">",
-    //     "<xs:attribute name=\"contentTypes\">",
-    // );
+    if std::env::var("DOCS_RS").is_err() {
+        const PROJECT_XSD: &str = include_str!("./dawproject/Project.xsd");
 
-    // // Write file
-    // std::fs::write("assets/Project.xsd", fixed_project_xsd).unwrap();
+        let fixed_project_xsd = PROJECT_XSD.replace(
+            "<xs:attribute name=\"contentType\">",
+            "<xs:attribute name=\"contentTypes\">",
+        );
+
+        // Write file
+        std::fs::write("assets/Project.xsd", fixed_project_xsd).unwrap();
+    }
 }
