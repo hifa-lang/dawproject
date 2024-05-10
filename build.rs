affa849721114f8d946e04c0b35034097afa21de
kurotakazuki <kabisakanakabi@gmail.com>
2024-05-10 18:21:00 +0900

    add: git submodule command

--- build.rs
+++ build.rs
@@ -1,5 +1,12 @@
+use std::process::Command;
+
 fn main() {
     if std::env::var("DOCS_RS").is_err() {
+        Command::new("git")
+            .args(&["submodule", "update", "--init", "--recursive"])
+            .output()
+            .expect("Sync submodules failed!");
+
         const PROJECT_XSD: &str = include_str!("./dawproject/Project.xsd");
 
         let fixed_project_xsd = PROJECT_XSD.replace(
