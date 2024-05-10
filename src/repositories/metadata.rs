f9864fcf8f36678674380631476535e133ee4a9e
kurotakazuki <kabisakanakabi@gmail.com>
2024-05-10 18:56:04 +0900

    update: xsd

--- src/repositories/metadata.rs
+++ src/repositories/metadata.rs
@@ -1,7 +1,5 @@
 use hifa_xml_schema_derive::XmlSchema;
 
 #[derive(Debug, XmlSchema)]
-#[xml_schema(
-    source = "https://raw.githubusercontent.com/hifa-lang/dawproject/main/assets/FixedMetaData.xsd"
-)]
+#[xml_schema(source = "assets/FixedMetaData.xsd")]
 struct MetadataSchema;
--- src/repositories/project.rs
+++ src/repositories/project.rs
@@ -2,7 +2,5 @@ use hifa_xml_schema_derive::XmlSchema;
 // use yaserde::{YaDeserialize, YaSerialize};
 
 #[derive(Debug, XmlSchema)]
-#[xml_schema(
-    source = "https://raw.githubusercontent.com/hifa-lang/dawproject/main/assets/FixedProject.xsd"
-)]
+#[xml_schema(source = "assets/FixedProject.xsd")]
 struct ProjectSchema;
